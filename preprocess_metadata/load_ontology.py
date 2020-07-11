9fb2d128f4983a82929d89805dba414fb067f25c
Matthew Bernstein <matthewbernstein@Matthews-MacBook-Pro-2.local>
2020-07-11 10:50:13 -0500

    Full Snakemake pipeline for generating static MetaSRA and SRAdb artifacts

--- preprocess_metadata/load_ontology.py
+++ preprocess_metadata/load_ontology.py
@@ -1,8 +1,13 @@
 from onto_lib_py3 import load_ontology as lo
 
-ONT_NAME_TO_ONT_ID = {"EFO_CL_DOID_UBERON_CVCL":"17"}
+ONT_NAME_TO_ONT_ID = {
+    "EFO_CL_DOID_UBERON_CVCL": "17",
+    "UO": "7"    
+}
 ONT_ID_TO_OG = {x: lo.load(x)[0] for x in ONT_NAME_TO_ONT_ID.values()}
-
 
 def the_ontology():
     return ONT_ID_TO_OG['17']
+
+def unit_ontology():
+    return ONT_ID_TO_OG['7']
